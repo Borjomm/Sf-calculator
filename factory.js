be318c78114e1d8c323f2420eeb27befe338f9dd
Borjomm <65873997+Borjomm@users.noreply.github.com>
2024-07-24 00:04:46 +0000

    Update factory.js

--- factory.js
+++ factory.js
@@ -19,7 +19,7 @@ import { BuildTarget } from "./target.js"
 import { Totals } from "./totals.js"
 import { renderTotals } from "./visualize.js"
 
-const DEFAULT_ITEM_KEY = "iron-plater"
+const DEFAULT_ITEM_KEY = "iron-plate"
 
 let minerCategories = new Set(["mineral", "oil", "water", "gas"])
 
